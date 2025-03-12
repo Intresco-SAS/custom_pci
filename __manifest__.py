1aa5372fe77f0f3484581085fbd413b2173a8dca
Nelson V <nvelez@intresco.co>
2025-03-12 12:38:02 -0500

    Odoo15

--- __manifest__.py
+++ __manifest__.py
@@ -17,16 +17,23 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '15.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['crm','sale','sale_crm'],
+    'depends': [
+        'crm',
+        'sale',
+        'sale_crm',
+        'purchase',
+        'purchase_order_line_menu',
+        ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         
         'views/sale_order.xml',
+        'views/purchase_views.xml',
         #'views/crm.xml',
         #'views/templates.xml',
     ],
